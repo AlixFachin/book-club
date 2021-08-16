4c68f61a30e22f1f18554f18e6f7f08a9aba434a
Alix Fachin <alix.fachin@me.com>
2021-08-16 21:23:45 +0900

    Adding test structure for API routes

--- be-book-club/src/tests/routes.test.ts
+++ be-book-club/src/tests/routes.test.ts
@@ -1,20 +1,37 @@
 import { Book } from "../entitites/BookEntity";
 import { getDBConnection } from "../db";
-import { Connection } from "typeorm"
+import { Connection, getConnection } from "typeorm"
+import App from "../server";
 
-import { describe } from "mocha"
-import { expect } from "chai"
+import "mocha";
+import chai from "chai";
+import chaiHttp from "chai-http";
+import { doesNotMatch } from "assert/strict";
+
+chai.use(chaiHttp);
+const expect = chai.expect;
 
 describe('Routes API Test', async () => {
     let DBConnection: Connection;
+    let server : App; 
 
     before(async () => {
-        DBConnection = await getDBConnection(true);
+        DBConnection = getConnection();
+        if (!DBConnection) {
+            DBConnection = await getDBConnection(true);
+        }
+        server = new App();
+        server.start();
     })
 
-    it('Should return the list of all packages', async () => {
+   describe("Book Entity Endpoints", () => {
 
-    })
+        it("Should have a GET access point", async () => {
+            const res = await chai.request(server.app).get("/api/v1/books");
+            expect(res).to.have.status(200);
 
+        });
+
+   })
 
 })
